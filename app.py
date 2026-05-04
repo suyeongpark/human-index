608bc32619d6749c7507f476f9d8e3a723068d18
suyeongpark <suyeong.park@bluedragons.kr>
2026-05-04 21:35:21 +0900

    fix: streamlit-analytics2 호환성 에러 수정
    streamlit-analytics2가 최신 Streamlit의 session_state와
    호환되지 않아 AttributeError 발생. analytics 래핑 제거.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Human Index Dashboard
 """
 
 import streamlit as st
-import streamlit_analytics2 as streamlit_analytics
+
 from lib.shared import run_query
 from lib import community, expert, combined
 
@@ -175,6 +175,5 @@ PAGE_MAP = {
 }
 
 page = st.session_state.page
-with streamlit_analytics.track():
-    if page in PAGE_MAP:
-        PAGE_MAP[page](start_date, end_date)
+if page in PAGE_MAP:
+    PAGE_MAP[page](start_date, end_date)
